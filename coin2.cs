49474bcdcfa2600a676010d7197b36d891ca4959
lizhihong <caffrey@trenddna.com>
2013-01-08 16:38:57 +0800

    test2

---
+++ coin2.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class coin2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+	 	animation["round"].speed = Random.Range(0.4f, 0.8f);
+	}
+	
+
+
+}
